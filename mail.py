414ef840b50601ad87d865612aa2149edb4f5abe
Jude Ogbonna <ogbonnajay@gmail.com>
2022-11-26 09:18:03 -0500

    added threaded emailing and a second emailing function for forex thread owner mail notification

--- mail.py
+++ mail.py
@@ -6,7 +6,6 @@ from flask_mail import Mail
 from switchfx import mail_obj
 
 bp = Blueprint('mail', __name__)
-
 def send_email(to, subject):
     msg = Message(
         subject,
@@ -19,6 +18,38 @@ def send_email(to, subject):
 @bp.route('/mail')
 def index():
     subject = 'This is a test mail'
-    to = 'ogbonnajay@gmail.com'
-    send_email('ogbonnajay@gmail.com', 'Another test mail for configuration')
-    return 'Mail sent to ogbonnajay@gmail Successfuly!'
+    to = 'ogbonnerrerajay@gmail.com'
+    send_email('ogbeeronnajay@gmail.com', 'Another test mail for configuration')
+    return 'Mail sent to ogboerernnajay@gmail Successfuly!'
+
+
+def mail_thread_owner(thread_owner_user_name, thread_owner_email, message):
+    subject = "Forex update for  %s " % thread_owner_user_name + "  "
+    msg = Message(recipients=[thread_owner_email],
+                        body=message,
+                        subject=subject)
+    mail_obj.send(msg)
+    
+ 
+def send_mails(users, message):
+    with mail_obj.connect() as conn:
+        for user in users:
+           # message = 'Follow the link to view'
+            subject = "Hello %s " % user['user_name'] + ", their is comment update" 
+            msg = Message(recipients=[user['email']],
+                        body=message,
+                        subject=subject)
+
+            conn.send(msg)
+
+def reset_mail(users, recover_url):
+    with mail_obj.connect() as conn:
+        for user in users:
+
+            message = "Hello %s " % user['first_name'] + ", please click on the link to reset your password.\r\n"+ recover_url
+            subject = "Password Reset for  %s " % user['first_name'] + "  "
+            msg = Message(recipients=[user['email']],
+                        body=message,
+                        subject=subject)
+
+            conn.send(msg)
